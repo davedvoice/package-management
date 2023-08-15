a6506b90775bafea3f08537a51d5197fae8c84a4
davedvoice <126035979+davedvoice@users.noreply.github.com>
2023-08-15 12:19:03 -0500

    Update install-tomcat.sh

--- Tomcat-installation/install-tomcat.sh
+++ Tomcat-installation/install-tomcat.sh
@@ -9,15 +9,15 @@ sudo yum install git wget -y
 sudo yum install java-1.8.0-openjdk-devel -y
 # Download tomcat software and extract it.
 # dowanload and extract tomcat software
-sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.69/bin/apache-tomcat-9.0.69.tar.gz
-sudo tar -xvf apache-tomcat-9.0.69.tar.gz
-sudo rm apache-tomcat-9.0.69.tar.gz
-sudo chmod 777 -R /opt/tomcat9
-sudo sh /opt/tomcat9/bin/startup.sh
+sudo wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.12/bin/apache-tomcat-10.1.12.tar.gz
+sudo tar -xvf apache-tomcat-10.1.12.tar.gz
+sudo rm apache-tomcat-10.1.12.tar.gz
+sudo chmod 777 -R /opt/tomcat10
+sudo sh /opt/tomcat10/bin/startup.sh
 # create a soft link to start and stop tomcat from anywhere 
 # This will enable you to manage tomcat as a service
-sudo ln -s /opt/tomcat9/bin/startup.sh /usr/bin/starttomcat
-sudo ln -s /opt/tomcat9/bin/shutdown.sh /usr/bin/stoptomcat
+sudo ln -s /opt/tomcat10/bin/startup.sh /usr/bin/starttomcat
+sudo ln -s /opt/tomcat10/bin/shutdown.sh /usr/bin/stoptomcat
 sudo starttomcat
 echo "end on tomcat installation"
 #========
